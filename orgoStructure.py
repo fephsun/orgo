ead24f9309c749885badd610b9957a6463eef768
Felix Sun <felixsun@mit.edu>
2013-01-09 18:07:23 -0500

    Streamlined basic structure - no more bond objects

--- orgoStructure.py
+++ orgoStructure.py
@@ -1,28 +1,21 @@
 class Molecule:
     def __init__(self, firstAtom):
         self.atoms = [firstAtom]
-        self.bonds = []
     def addAtom(self, newAtom, targetAtom, bondOrder):
         self.atoms.append(newAtom)
-        self.bonds.append(Bond(newAtom, targetAtom, bondOrder))
+        self.addBond(newAtom, targetAtom, bondOrder)
     def addBond(self, atom1, atom2, bondOrder):
-        self.bonds.append(Bond(atom1, atom2, bondOrder))
-    def removeAtom(self, atom):
-        for bond in self.bonds:
-            if bond.end1 == atom or bond.end2 == atom:
-                self.bonds.remove(bond)
-        self.atoms.remove(atom)
+        atom1.neighbors[atom2] = bondOrder
+        atom2.neighbors[atom1] = bondOrder
+    def removeAtom(self, target):
+        for atom in self.atoms:
+            if target in atom.neighbors:
+                del(atom.neighbors[target])
+        self.atoms.remove(target)
+        del(target)
     
     
 class Atom:
     def __init__(self, element):
         self.element = element
-
-class Bond:
-    def __init__(self, end1, end2, order):
-        self.end1 = end1
-        self.end2 = end2
-        self.order = order
-
-
-//test
+        self.neighbors = dict()

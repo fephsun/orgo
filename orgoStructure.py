180fd87d7c58a45493df4ee9cda25a2926378c6b
csvoss <csvoss@mit.edu>
2013-01-09 21:13:32 -0500

    more smiles function modifications

--- orgoStructure.py
+++ orgoStructure.py
@@ -45,7 +45,7 @@ class Atom:
         self.element = element
         self.neighbors = dict()
 
-        #Temporary values which should only be meaningful within smiles.
+        #Temporary values which should only be meaningful within smiles() and subsmiles().
         #for traversing
         self.flag = 0
         #for ring-finding
@@ -53,6 +53,9 @@ class Atom:
         self.rAtom = 0 #0 if not part of a ring bond, the other-atom the bond is made with if otherwise
         self.nRead = 0 #neighbors already read
         self.parentAtom = 0 #atom right before this one
+        self.nonHNeighbors = []
+
+
 
     def newChiralCenter(self, reference, clockwiseList):
         #Set up this atom as a chiral center.
@@ -80,29 +83,34 @@ def smiles(molecule):
     curAtom = molecule.atoms[0]
     homeAtom = molecule.atoms[0]
 
+    #Create the dictionary nonHNeighbors for each atom.
+    for atom in molecule.atoms:
+        atom.nonHNeighbors = dict((a, b) for (a, b)in atom.neighbors.items() if a.element.lower() != "h"
+    
+
     #Traverse the molecule once, to hunt down and flag rings.
     #Each iteration: (...while we aren't back to the home atom, or if we are,
                     #while the home atom still has neighbors to read)
-    while ((curAtom != homeAtom) or (homeAtom.nRead < len(homeAtom.neighbors))):
+    while ((curAtom != homeAtom) or (homeAtom.nRead < len(homeAtom.nonHNeighbors))):
     
         #flag current atom as "read" (flag = 1)
         curAtom.flag = 1
         
         #if there are neighbors left to read from this atom:
-        if (curAtom.nRead < len(curAtom.neighbors)):
+        if (curAtom.nRead < len(curAtom.nonHNeighbors)):
             
             #if the next atom is the parent atom:
-            if list(curAtom.neighbors)[curAtom.nRead] == curAtom.parentAtom:
+            if list(curAtom.nonHNeighbors)[curAtom.nRead] == curAtom.parentAtom:
                 #don't do anything but incrementing nRead
                 curAtom.nRead += 1
                 
             #else,
             else:
-                if list(curAtom.neighbors)[curAtom.nRead].nRead == 0:
+                if list(curAtom.nonHNeighbors)[curAtom.nRead].nRead == 0:
                 #if the next atom has not been traversed yet:
                     curAtom.nRead += 1
-                    list(curAtom.neighbors)[curAtom.nRead - 1].parentAtom = curAtom
-                    curAtom = list(curAtom.neighbors)[curAtom.nRead - 1]
+                    list(curAtom.nonHNeighbors)[curAtom.nRead - 1].parentAtom = curAtom
+                    curAtom = list(curAtom.nonHNeighbors)[curAtom.nRead - 1]
                     #increment current atom's nRead counter
                     #make the next atom the current atom:
                     #make the old atom the next atom's parent
@@ -112,10 +120,10 @@ def smiles(molecule):
                     #it's a ring!
                     ringsfound += 1
                     curAtom.rflag = ringsfound
-                    list(curAtom.neighbors)[curAtom.nRead].rflag = ringsfound
+                    list(curAtom.nonHNeighbors)[curAtom.nRead].rflag = ringsfound
                     curAtom.nRead += 1
-                    curAtom.rAtom = list(curAtom.neighbors)[curAtom.nRead - 1]
-                    list(curAtom.neighbors)[curAtom.nRead - 1].rAtom = curAtom
+                    curAtom.rAtom = list(curAtom.nonHNeighbors)[curAtom.nRead - 1]
+                    list(curAtom.nonHNeighbors)[curAtom.nRead - 1].rAtom = curAtom
                     #increment ringsfound
                     #set rflag on both atoms to ringsfound
                     #increment current atom's nRead counter
@@ -142,19 +150,24 @@ def smiles(molecule):
     return outp
 
 bondSymbols = ['0', '-', '=', '#', '4', '5', '6', '7', '8', '9']
-#Precondition: molecule has been flagged for ring positioning (some rflag values on atoms might != 0)
+
+#Precondition: molecule has been flagged for ring positioning (some rflag values on atoms might != 0). This is done by smiles().
 def subsmiles(molecule, startAtom, parentAtom):
     #Creates and returns a SMILES string for unflagged (!atom.flag==2) atoms within a molecule, starting with the given atom
 
     outp = startAtom.element
+
+    #Check if the molecule is a chiral center.
 
     #If the molecule has an rflag, find the neighbor it bonds with.
     #If that neighbor has already been traversed, add the rflag while specifying the bond.
     #If not, just add the rflag.
     if startAtom.rflag != 0:
         if startAtom.rAtom.flag == 1:
-            outp += bondSymbols[startAtom.neighbors[startAtom.rAtom]]
+            outp += bondSymbols[startAtom.nonHNeighbors[startAtom.rAtom]]
         outp += str(startAtom.rflag)
+
+    
     
     #Flag the current atom.
     startAtom.flag = 1
@@ -162,17 +175,17 @@ def subsmiles(molecule, startAtom, parentAtom):
     #Add new groups for all neighbor atoms which are not the parent atom and not the rAtom.
     #Recursion is your friend.
     #Be sure to specify the base case (when zero non-parent non-ring atoms are available to bond to)
-    toAdd = [atom for atom in list(startAtom.neighbors) if not (atom==startAtom.rAtom or atom==parentAtom)]
-
+    toAdd = [atom for atom in list(startAtom.nonHNeighbors) if not (atom==startAtom.rAtom or atom==parentAtom)]
+    
     #In the base case, this loop won't even be entered.
     for atom in toAdd:
-        outp += "(" +bondSymbols[startAtom.neighbors[atom]] + subsmiles(molecule, atom, startAtom) + ")" 
+        outp += "(" +bondSymbols[startAtom.nonHNeighbors[atom]] + subsmiles(molecule, atom, startAtom) + ")" 
     return outp
 
 
 #Makes     C-C-C-C
 #          |   |
-#       HO-C=C-N
+#        O-C=C-N
 c1 = Atom("C")
 mol = Molecule(c1)
 c2 = Atom("C")
@@ -180,9 +193,7 @@ n1 = Atom("N")
 mol.addAtom(c2, c1, 2)
 mol.addAtom(n1, c2, 1)
 o1 = Atom("O")
-h1 = Atom("H")
 mol.addAtom(o1, c1, 1)
-mol.addAtom(h1, o1, 1)
 
 c3 = Atom("C")
 mol.addAtom(c3, n1, 1)

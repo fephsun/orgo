fc8286f6fc3f193c34fcb0c86f87f790ff022e82
csvoss <csvoss@mit.edu>
2013-01-20 15:16:04 -0500

    Added code to convert reagents to html on the server side instead

--- engine/synthProblem.py
+++ engine/synthProblem.py
@@ -172,6 +172,20 @@ class MoleculeBox:
 #    def stringList(self):
 #        return [REAGENTS[reagent][0] for reagent in list(self.hasReagents) if self.hasReagents[reagent]] 
  
+ 
+ 
+def returnReagentHtml(request):
+    reagentString = request.reagentString
+    dict = parseReagentsString(reagentString)
+    html = ""
+    
+    for reagent in list(dict):
+        if dict[reagent]:
+            html += REAGENTS[reagent][0] + ", "
+            
+    htmlOutput =  "<li class=\"reagent\" class = \"ui-state-default\" reagentString = \""+reagentString+"\">"+html[:-2]+"<img src=\"http://felixsun.scripts.mit.edu/orgo/static/arrow.png\"/></li>"
+
+    return HttpResponse(htmlOutput)
       
 #Enter a string, such as "H2 cat Pd|C"
 #Returns a dictionary, such as {"H2":True, "PDC":True, "ETOH":False, ...}
